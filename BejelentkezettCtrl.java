5a2d0b1e2a3dc7cfa2d3c4e03afeb000ff2dfc34
farkastomi01 <122123045+farkastomi01@users.noreply.github.com>
2023-12-06 21:00:20 +0100

    Add files via upload
    Ebben a kód részletben a felhasználói adatokat jeleníti meg a kód, mi a neve a felhasználónak, a kedvenc Youtube csatornája.

---
+++ BejelentkezettCtrl.java
@@ -0,0 +1,35 @@
+package com.example.loginsetup;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BejelentkezettCtrl implements Inizializable {
+    @FXML
+    private Button button_kijelentkezes;
+    @FXML
+    private Label lbl_udvozles;
+    @FXML
+   private  Label lbl_felirat;
+
+
+public void Inicializalas(URL location, ResourceBundle resources)
+{
+ button_kijelentkezes.setOnAction(new EventHandler<ActionEvent>() {
+     @Override
+     public void handle(ActionEvent actionEvent) {
+
+     }
+ });
+}
+public void felhasznaloInformacioBeallitas(String felhasznalonev,String kedvencCsatorna)
+{
+    lbl_udvozles.setText("Üdvözlöm"+felhasznalonev+"!");
+    lbl_felirat.setText("Az Ön kedvenc YouTube csatornája"+kedvencCsatorna+"!");
+}
+}
